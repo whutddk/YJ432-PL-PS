e9b66066a1e19ab54abdc22460c956be09dc9a1b
WUT_Ruige_Lee <295054118@whut.edu.cn>
2018-06-19 16:02:48 +0800

    问题出现在byte摸得、
    挂载在0x60000000上，开CLKOUT
    
    挂6000000上,0f掩盖，7ffff读写通过
    
    为YJ432准备
    
    添加freecar的代码
    
    建立bzled
    
    蜂鸣器没准备好
    
    QEI可以使用，码盘精度太低，一圈32
    
    基本强控制倒立完成
    
    编码器精度太小，或许不适合做并联环
    
    再次尝试使用串联
    
    正负号写反了
    
    有收敛的趋势，阻尼明显不足
    
    系统在高频振荡中收敛，高度怀疑是电机驱动不行，软件控制得到论证
    
    操作fb_bzled
    
    开始调PWM 双fb
    
    pwm测试
    
    ei计数及清零测试通过，bug 清零时不稳定
    
    PS-PL联合测试
    
    PS+PL旋转倒立摆收敛

---
+++ UR_LIB/CTL/motor.cpp
@@ -0,0 +1,30 @@
+#include "mbed.h"
+
+#include "fsl_common.h"
+#include "fsl_port.h"
+#include "fsl_ftm.h"
+
+#include "include.h"
+
+
+
+void motor_init()
+{
+	* (PWM0_FRE_REG) = 10000;
+	* (PWM0_CH0_REG) = 1;
+	* (PWM0_CH1_REG) = 1;
+
+	* (QEI0_CLEAR_REG) = 1;
+	* (QEI0_CLEAR_REG) = 0;
+}
+
+int32_t QEI1;
+void get_qei()
+{
+	QEI1 = (int32_t)(*(QEI0_CH0_REG));
+}
+
+
+
+
+
