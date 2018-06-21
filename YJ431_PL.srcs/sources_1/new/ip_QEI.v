292126e0c9eb3fb6b7dfe2265ba34f5fbd221a3f
WUT_Ruige_Lee <295054118@whut.edu.cn>
2018-06-21 17:45:46 +0800

    for furture!

---
+++ YJ431_PL.srcs/sources_1/new/ip_QEI.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2018/06/21 17:44:39
+// Design Name: 
+// Module Name: ip_QEI
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module ip_QEI(
+
+    );
+endmodule
