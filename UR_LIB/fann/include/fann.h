da7704d41dc1b73f20f782c03b99be4282e82b89
WUT_Ruige_Lee <295054118@whut.edu.cn>
2018-07-08 11:08:48 +0800

    清理代码

--- UR_LIB/fann/include/fann.h
+++ UR_LIB/fann/include/fann.h
@@ -46,18 +46,8 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 	
 #else
 	
-/* COMPAT_TIME REPLACEMENT */ 
-// #ifndef _WIN32
 #include <sys/time.h>
-// #else	/* _WIN32 */
-// #if !defined(_MSC_EXTENSIONS) && !defined(_INC_WINDOWS)  
-// extern unsigned long __stdcall GetTickCount(void);
 
-// #else	 _MSC_EXTENSIONS 
-// #define WIN32_LEAN_AND_MEAN
-// #include <windows.h>
-// #endif	/* _MSC_EXTENSIONS */
-// #endif	/* _WIN32 */
 		
 #ifndef __fann_h__
 #define __fann_h__
@@ -74,68 +64,6 @@ extern "C"
 #ifndef NULL
 #define NULL 0
 #endif	/* NULL */
- 
-/* ----- Macros used to define DLL external entrypoints ----- */ 
-/*
- DLL Export, import and calling convention for Windows.
- Only defined for Microsoft VC++ FANN_EXTERNAL indicates
- that a function will be exported/imported from a dll
- ensures that the DLL calling convention
- will be used for  a function regardless of the calling convention
- used when compiling.
-
- For a function to be exported from a DLL its prototype and
- declaration must be like this:
-    FANN_EXTERNAL void function(char *argument)
-
- The following ifdef block is a way of creating macros which
- make exporting from a DLL simple. All files within a DLL are
- compiled with the FANN_DLL_EXPORTS symbol defined on the
- command line. This symbol should not be defined on any project
- that uses this DLL. This way any other project whose source
- files include this file see FANN_EXTERNAL functions as being imported
- from a DLL, whereas a DLL sees symbols defined with this
- macro as being exported which makes calls more efficient.
- The __stdcall calling convention is used for functions in a
- windows DLL.
-
- The callback functions for fann_set_callback must be declared as FANN_API
- so the DLL and the application program both use the same
- calling convention.
-*/ 
- 
-/*
- The following sets the default for MSVC++ 2003 or later to use
- the fann dll's. To use a lib or fixedfann.c, floatfann.c or doublefann.c
- with those compilers FANN_NO_DLL has to be defined before
- including the fann headers.
- The default for previous MSVC compilers such as VC++ 6 is not
- to use dll's. To use dll's FANN_USE_DLL has to be defined before
- including the fann headers.
-*/ 
-// #if (_MSC_VER > 1300)
-
-// #ifndef FANN_NO_DLL
-
-// #define FANN_USE_DLL
-
-// #endif	/* FANN_USE_LIB */
-// #endif	/* _MSC_VER */
-// 
-// #if defined(_MSC_VER) && (defined(FANN_USE_DLL) || defined(FANN_DLL_EXPORTS))
-
-// #ifdef FANN_DLL_EXPORTS
-// #define FANN_EXTERNAL __declspec(dllexport)
-// #else							/*  */
-// #define FANN_EXTERNAL __declspec(dllimport)
-	
-// #endif	/* FANN_DLL_EXPORTS*/
-// #define __stdcall
-// #else							/*  */
-// #define FANN_EXTERNAL
-// #define FANN_API
-// #endif	/* _MSC_VER */
-/* ----- End of macros used to define DLL external entrypoints ----- */ 
 
 #include "fann_error.h"
 #include "fann_activation.h"
