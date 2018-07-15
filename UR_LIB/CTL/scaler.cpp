9d8fce9b812b05497d70fe21c7be10e6c3139b28
WUT_Ruige_Lee <295054118@whut.edu.cn>
2018-07-15 21:23:19 +0800

    基本测试完PID加速，问题：参数变了，软件环加不上去

--- UR_LIB/CTL/scaler.cpp
+++ UR_LIB/CTL/scaler.cpp
@@ -3,21 +3,20 @@
 float Fix2Float(int32_t Fix_data)
 {
 	float fl_data;
-	bool nege = 0;
+	// bool nege = 0;
 
-	if ( Fix_data < 0 )
-	{
-		Fix_data = -Fix_data;
-		nege = 1;
-	}
+	// if ( Fix_data < 0 )
+	// {
+	// 	Fix_data = -Fix_data;
+	// 	nege = 1;
+	// }
 
-	fl_data = Fix_data >> 15;
-	fl_data += ( Fix_data & 0x7fff ) / 32768.;
+	fl_data = Fix_data / 32768.;
 
-	if ( nege )
-	{
-		fl_data = -fl_data;
-	}
+	// if ( nege )
+	// {
+	// 	fl_data = -fl_data;
+	// }
 
 	return fl_data;
 }
@@ -25,21 +24,21 @@ float Fix2Float(int32_t Fix_data)
 int32_t Float2Fix(float fl_data )
 {
 	int32_t fix_data;
-	bool nege = 0;
+	// bool nege = 0;
 
-	if ( fl_data < 0 )
-	{
-		fl_data = -fl_data;
-		nege = 1;
-	}
+	// if ( fl_data < 0 )
+	// {
+	// 	fl_data = -fl_data;
+	// 	nege = 1;
+	// }
 
-	fix_data = ( (int32_t)(fl_data) ) << 15;
-	fix_data += ( fl_data - fix_data ) * 32768 ;
 
-	if ( nege )
-	{
-		fix_data = -fix_data;
-	}
+	fix_data = fl_data * 32768 ;
+
+	// if ( nege )
+	// {
+	// 	fix_data = -fix_data;
+	// }
 
 	return fix_data;
 }
--- UR_LIB/ITAC_TASK/freecars.cpp
+++ UR_LIB/ITAC_TASK/freecars.cpp
@@ -58,23 +58,27 @@ void sendDataToScope()
 uint8_t flag_receive = 0;
 static void UartDebug()
 {
-	ctl.motto.Kp_s = UartData[0];
-	ctl.motto.Kd_s = UartData[1];
-	ctl.motto.Kp_m = UartData[2];
-	ctl.motto.Kd_m = UartData[3];
-	ctl.motto.Kp_b = UartData[4];
-	ctl.motto.Kd_b = UartData[5];
+	// ctl.motto.Kp_s = UartData[0];
+	// ctl.motto.Kd_s = UartData[1];
+	// ctl.motto.Kp_m = UartData[2];
+	// ctl.motto.Kd_m = UartData[3];
+	// ctl.motto.Kp_b = UartData[4];
+	// ctl.motto.Kd_b = UartData[5];
 
+	*(po3PID0_KPS_REG) = Float2Fix(UartData[0]);
+	*(po3PID0_KDS_REG) = Float2Fix(UartData[1]);
+	*(po3PID0_KPM_REG) = Float2Fix(UartData[2]);
+	*(po3PID0_KDM_REG) = Float2Fix(UartData[3]);
 	// ctl.pend.Kp_m = 6;//UartData[2]; 
 	// ctl.pend.Kd_m = 208;//UartData[3];
 		
 	// ctl.pend.Kp_b = 6;//UartData[4]; 
 	// ctl.pend.Kd_b = 208;//UartData[5];
 
-	ctl.pend.aim =(int32_t)( UartData[6]*1000);
+	// ctl.pend.aim =(int32_t)( UartData[6]*1000);
 
-	ctl.pend.Kp_s = ctl.pend.Kp_m = ctl.pend.Kp_b = UartData[7];
-	ctl.pend.Kd_s = ctl.pend.Kd_m = ctl.pend.Kd_b = UartData[8];
+	ctl.motto.Kp_s = ctl.motto.Kp_m = ctl.motto.Kp_b = UartData[7];
+	ctl.motto.Kd_s = ctl.motto.Kd_m = ctl.motto.Kd_b = UartData[8];
 
 	// ctl.motto.Kp_m = UartData[2];
 	// ctl.motto.Kd_m = UartData[3];
