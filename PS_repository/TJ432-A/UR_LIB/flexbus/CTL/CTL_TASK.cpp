0940951ee76ae20753e4b57d454298399e62740c
WUT_Ruige_Lee <295054118@whut.edu.cn>
2018-07-17 10:34:37 +0800

    compile pass

--- PS_repository/TJ432-A/UR_LIB/flexbus/CTL/CTL_TASK.cpp
+++ PS_repository/TJ432-A/UR_LIB/flexbus/CTL/CTL_TASK.cpp
@@ -29,94 +29,60 @@ void get_pos()
 void control()
 {
 	* (po3PID0_CUR_REG) = ctl.pend.cur;
+	* (po3PID1_CUR_REG) = QEI1;
+
 	
+	//GET PID1 RESULT
+	ctl.motto.result = Fix2Float( *po3PID1_OUT_REG );
 
-	if ( parallel_cnt == 2 )		//位置控制 位置式
+	push( 4,(int16_t)(ctl.motto.result) );
+
+	if ( ctl.motto.result > 5000.00 )
 	{
-		parallel_cnt = 0;
-
-		ctl.motto.error[0] = ctl.motto.cur = (double)(QEI1 / 1.0);
-
-
-		// if ( ctl.motto.error[0] < 100.00 && ctl.motto.error[0] > -100.00 )
-		// {
-		// 	ctl.motto.result = 0;
-		// 	ctl.motto.sum = 0 ;
-		// }
-		// else 
-			if ( ctl.motto.error[0] < 300.00 && ctl.motto.error[0] > -300.00 )
-		{
-			ctl.motto.result = ctl.motto.Kp_s * ctl.motto.error[0];
-
-			ctl.motto.result += ctl.motto.Kd_s * ( ctl.motto.error[0] - ctl.motto.error[1] );
-		}
-		else if ( ctl.motto.error[0] < 600.00 && ctl.motto.error[0] > -600.00 )
-		{
-			ctl.motto.result = ctl.motto.Kp_m * ctl.motto.error[0];
-
-			ctl.motto.result += ctl.motto.Kd_m * ( ctl.motto.error[0] - ctl.motto.error[1] );
-		}		
-		else 
-		{
-			ctl.motto.result = ctl.motto.Kp_b * ctl.motto.error[0];
-
-			ctl.motto.result += ctl.motto.Kd_b * ( ctl.motto.error[0] - ctl.motto.error[1] );
-		}
-
-		ctl.motto.error[3] = ctl.motto.error[2];
-		ctl.motto.error[2] = ctl.motto.error[1];
-		ctl.motto.error[1] = ctl.motto.error[0];
-
-		push( 4,(int16_t)(ctl.motto.result) );
-
-		if ( ctl.motto.result > 5000.00 )
-		{
-			ctl.motto.result = 5000.00;
-		}
-		else if ( ctl.motto.result < -5000.00 )
-		{
-			ctl.motto.result = -5000.00;
-		}
-
+		ctl.motto.result = 5000.00;
 	}
-	parallel_cnt ++;
-	
-	// if ( parallel_cnt == 5 )
+	else if ( ctl.motto.result < -5000.00 )
 	{
-		ctl.pend.result =  Fix2Float( *po3PID0_OUT_REG ); //get PID result
-		ctl.out = (int32_t)( ctl.pend.result - ctl.motto.result ) ;
+		ctl.motto.result = -5000.00;
+	}
+
+	
+	//GET PID0 RESULT
+	ctl.pend.result =  Fix2Float( *po3PID0_OUT_REG ); 
+	ctl.out = (int32_t)( ctl.pend.result - ctl.motto.result ) ;
 
 	
 
-		if ( ctl.flag_end == 1 )
-		{	
-			* (PWM0_CH0_REG) = 1;
+// OUTPUT PWM0
+	if ( ctl.flag_end == 1 )
+	{	
+		* (PWM0_CH0_REG) = 1;
+		* (PWM0_CH1_REG) = 1;
+	}
+	else
+	{
+		if ( ctl.out >= 10000 )
+		{
+			* (PWM0_CH0_REG) = 9999;
 			* (PWM0_CH1_REG) = 1;
+		}
+		else if ( ctl.out > 0 )
+		{
+			* (PWM0_CH0_REG) = (uint32_t)(ctl.out);
+			* (PWM0_CH1_REG) = 1;
+		}
+		else if ( ctl.out > -10000 )
+		{
+			* (PWM0_CH0_REG) = 1;
+			* (PWM0_CH1_REG) = (uint32_t)(-ctl.out);
 		}
 		else
 		{
-			if ( ctl.out >= 10000 )
-			{
-				* (PWM0_CH0_REG) = 9999;
-				* (PWM0_CH1_REG) = 1;
-			}
-			else if ( ctl.out > 0 )
-			{
-				* (PWM0_CH0_REG) = (uint32_t)(ctl.out);
-				* (PWM0_CH1_REG) = 1;
-			}
-			else if ( ctl.out > -10000 )
-			{
-				* (PWM0_CH0_REG) = 1;
-				* (PWM0_CH1_REG) = (uint32_t)(-ctl.out);
-			}
-			else
-			{
-				* (PWM0_CH0_REG) = 1;
-				* (PWM0_CH1_REG) = 9999;
-			}
+			* (PWM0_CH0_REG) = 1;
+			* (PWM0_CH1_REG) = 9999;
 		}
 	}
+
 
 }
 
