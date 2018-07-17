29f1f2ff4bc4e800d6ed3f2e61280ef6e00a536b
WUT_Ruige_Lee <295054118@whut.edu.cn>
2018-07-17 10:22:02 +0800

    register PID1

--- UR_LIB/CTL/motor.cpp
+++ UR_LIB/CTL/motor.cpp
@@ -17,27 +17,49 @@ void motor_init()
 	* (QEI0_CLEAR_REG) = 1;
 	* (QEI0_CLEAR_REG) = 0;
 
-//注册PID频率
+//注册po3_PID0频率
 	* (po3PID0_FREQ_REG) = 250000;  	//5ms
 
-//初始化目标和当前值
+//初始化po3_PID0目标和当前值
 	* (po3PID0_AIM_REG) = 24200;
 	* (po3PID0_CUR_REG) = 0;
 
-//初始化小偏差(CONTROL)
-	* (po3PID0_ERS_REG) = 3000;
+//初始化po3_PID0小偏差(CONTROL)
+	* (po3PID0_ERS_REG) = 100;
 	* (po3PID0_KPS_REG) = Float2Fix(3.00);
 	* (po3PID0_KDS_REG) = Float2Fix(462.00);
 
-//初始化中偏差
-	* (po3PID0_ERM_REG) = 10000;
+//初始化po3_PID0中偏差
+	* (po3PID0_ERM_REG) = 3000;
 	* (po3PID0_KPM_REG) = Float2Fix(4.00);
 	* (po3PID0_KDM_REG) = Float2Fix(472.00);
 
-//初始化大偏差(NOT CONTROL)
-	* (po3PID0_ERB_REG) = 20000;
-	* (po3PID0_KPB_REG) = 0;
-	* (po3PID0_KDB_REG) = 0;
+//初始化po3_PID0大偏差
+	* (po3PID0_ERB_REG) = 5000;
+	* (po3PID0_KPB_REG) = Float2Fix(4.00);
+	* (po3PID0_KDB_REG) = Float2Fix(472.00);
+
+//注册po3_PID1频率
+	* (po3PID1_FREQ_REG) = 500000;  	//10ms
+
+//初始化po3_PID1目标和当前值
+	* (po3PID1_AIM_REG) = 24200;
+	* (po3PID1_CUR_REG) = 0;
+
+//初始化po3_PID1小偏差(CONTROL)
+	* (po3PID1_ERS_REG) = 10;
+	* (po3PID1_KPS_REG) = Float2Fix(2.00);
+	* (po3PID1_KDS_REG) = Float2Fix(222.00);
+
+//初始化po3_PID1中偏差
+	* (po3PID1_ERM_REG) = 300;
+	* (po3PID1_KPM_REG) = Float2Fix(2.00);
+	* (po3PID1_KDM_REG) = Float2Fix(222.00);
+
+//初始化po3_PID1大偏差
+	* (po3PID1_ERB_REG) = 600;
+	* (po3PID1_KPB_REG) = Float2Fix(2.00);
+	* (po3PID1_KDB_REG) = Float2Fix(472.00);
 
 }
 
--- UR_LIB/CTL/scaler.cpp
+++ UR_LIB/CTL/scaler.cpp
@@ -3,20 +3,8 @@
 float Fix2Float(int32_t Fix_data)
 {
 	float fl_data;
-	// bool nege = 0;
-
-	// if ( Fix_data < 0 )
-	// {
-	// 	Fix_data = -Fix_data;
-	// 	nege = 1;
-	// }
 
 	fl_data = Fix_data / 32768.;
-
-	// if ( nege )
-	// {
-	// 	fl_data = -fl_data;
-	// }
 
 	return fl_data;
 }
@@ -24,21 +12,8 @@ float Fix2Float(int32_t Fix_data)
 int32_t Float2Fix(float fl_data )
 {
 	int32_t fix_data;
-	// bool nege = 0;
-
-	// if ( fl_data < 0 )
-	// {
-	// 	fl_data = -fl_data;
-	// 	nege = 1;
-	// }
-
 
 	fix_data = fl_data * 32768 ;
-
-	// if ( nege )
-	// {
-	// 	fix_data = -fix_data;
-	// }
 
 	return fix_data;
 }
