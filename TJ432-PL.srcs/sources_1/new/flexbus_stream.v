ff412a373939c73f009081875bc841019d8063c4
WUT_Ruige_Lee <295054118@whut.edu.cn>
2018-07-19 10:38:22 +0800

    init project

---
+++ TJ432-PL.srcs/sources_1/new/flexbus_stream.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2018/07/19 10:35:01
+// Design Name: 
+// Module Name: flexbus_stream
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module flexbus_stream(
+    input CLK
+    );
+endmodule
