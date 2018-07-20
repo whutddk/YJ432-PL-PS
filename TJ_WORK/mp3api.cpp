e350a679f5a869a0fe2e4d6c6ba30b8fdd96f709
WUT_Ruige_Lee <295054118@whut.edu.cn>
2018-07-20 19:59:00 +0800

    理论的wav播放电路

--- TJ_WORK/mp3api.cpp
+++ TJ_WORK/mp3api.cpp
@@ -48,102 +48,110 @@ void play_mp3(char* filename)
 		fc.printf(" done.\r\n");
 	}
 
-	
 
-	fread(buf_read,1,NUM_BYTE_READ,fd);
-	fc.printf("start to trace\r\n");
-	int32_t flag_start = 0;
-
-	while(1)
+	num_read = 1;
+	while(num_read)
 	{
-		offset = MP3FindSyncWord(read_ptr, byte_left);
+		num_read = fread((void*)TJBMP3_reg,1,NUM_BYTE_READ,fd);
 
-		if (offset < 0 ) 
-		{
-
-			if( flag_start == 0 )
-			{
-				fread(buf_read,1,NUM_BYTE_READ,fd);
-				if ( num_read == 0 )
-				{
-					break;
-				}
-				continue;
-			}
-			else
-			{
-				break;
-			}
-		} 
-		else 
-		{
-			flag_start = 1;
-			read_ptr += offset;         //data start point
-			byte_left -= offset;        //in buffer
-
-			if ( flag_sw == 0 )
-			{
-				
-				res_dec = MP3Decode(mp3_dec, &read_ptr, (int *)&byte_left, buf_rec1, 0);
-				
-			}
-			else
-			{
-				
-				res_dec = MP3Decode(mp3_dec, &read_ptr, (int *)&byte_left, buf_rec2, 0);
-				
-			}
-
-			if (res_dec != ERR_MP3_NONE)
-			{
-				fc.printf("MP3Decode error:%d!\n\r",res_dec);
-				read_ptr += 2;
-				flag_start = 0;
-				num_read = fread(buf_read,1,NUM_BYTE_READ,fd);
-				if ( num_read == 0 )
-				{
-					break;
-				}
-				continue;
-			}
-
-
-		    if ( flag_sw == 0 )
-		    {
-		        temp = (uint32_t)buf_rec1;
-		        flag_sw = 1;
-		    }
-		    else
-		    {
-		        temp = (uint32_t)buf_rec2;
-		        flag_sw = 0;
-		    }
-
-		    /********************************
-
-			transfer here!!!
-
-			*******************************/
-			if (byte_left < NUM_BYTE_READ) 
-			{
-				memmove(buf_read,read_ptr,byte_left);
-
-				num_read = fread(buf_read + byte_left,1,NUM_BYTE_READ - byte_left,fd);
-
-				if(num_read == 0) 
-				{
-					fc.printf("num_read:%d\r\n",num_read);
-					break;
-				}
-				if ( num_read < NUM_BYTE_READ - byte_left )
-				{
-					memset(buf_read + byte_left + num_read, 0, NUM_BYTE_READ - byte_left - num_read);
-				}
-				byte_left = NUM_BYTE_READ;
-				read_ptr = buf_read;
-			}
-		}
+		while(!(*TJBMP3_IsEMPTY_REG));
 	}
+
+	fc.printf("start to trace\r\n");
+
+
+	// int32_t flag_start = 0;
+
+	// while(1)
+	// {
+	// 	offset = MP3FindSyncWord(read_ptr, byte_left);
+
+	// 	if (offset < 0 ) 
+	// 	{
+
+	// 		if( flag_start == 0 )
+	// 		{
+	// 			fread(buf_read,1,NUM_BYTE_READ,fd);
+	// 			if ( num_read == 0 )
+	// 			{
+	// 				break;
+	// 			}
+	// 			continue;
+	// 		}
+	// 		else
+	// 		{
+	// 			break;
+	// 		}
+	// 	} 
+	// 	else 
+	// 	{
+	// 		flag_start = 1;
+	// 		read_ptr += offset;         //data start point
+	// 		byte_left -= offset;        //in buffer
+
+	// 		if ( flag_sw == 0 )
+	// 		{
+				
+	// 			res_dec = MP3Decode(mp3_dec, &read_ptr, (int *)&byte_left, buf_rec1, 0);
+				
+	// 		}
+	// 		else
+	// 		{
+				
+	// 			res_dec = MP3Decode(mp3_dec, &read_ptr, (int *)&byte_left, buf_rec2, 0);
+				
+	// 		}
+
+	// 		if (res_dec != ERR_MP3_NONE)
+	// 		{
+	// 			fc.printf("MP3Decode error:%d!\n\r",res_dec);
+	// 			read_ptr += 2;
+	// 			flag_start = 0;
+	// 			num_read = fread(buf_read,1,NUM_BYTE_READ,fd);
+	// 			if ( num_read == 0 )
+	// 			{
+	// 				break;
+	// 			}
+	// 			continue;
+	// 		}
+
+
+	// 	    if ( flag_sw == 0 )
+	// 	    {
+	// 	        temp = (uint32_t)buf_rec1;
+	// 	        flag_sw = 1;
+	// 	    }
+	// 	    else
+	// 	    {
+	// 	        temp = (uint32_t)buf_rec2;
+	// 	        flag_sw = 0;
+	// 	    }
+
+	// 	    /********************************
+
+	// 		transfer here!!!
+
+	// 		*******************************/
+	// 		if (byte_left < NUM_BYTE_READ) 
+	// 		{
+	// 			memmove(buf_read,read_ptr,byte_left);
+
+	// 			num_read = fread(buf_read + byte_left,1,NUM_BYTE_READ - byte_left,fd);
+
+	// 			if(num_read == 0) 
+	// 			{
+	// 				fc.printf("num_read:%d\r\n",num_read);
+	// 				break;
+	// 			}
+	// 			if ( num_read < NUM_BYTE_READ - byte_left )
+	// 			{
+	// 				memset(buf_read + byte_left + num_read, 0, NUM_BYTE_READ - byte_left - num_read);
+	// 			}
+	// 			byte_left = NUM_BYTE_READ;
+	// 			read_ptr = buf_read;
+	// 		}
+	// 	}
+	// }
 	MP3FreeDecoder(mp3_dec);
 	fclose(fd);
 
