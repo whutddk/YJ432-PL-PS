804b6ae8fac985dcd436953030536c74da010dff
WUT_Ruige_Lee <295054118@whut.edu.cn>
2018-07-20 17:01:58 +0800

    扩充I2S到16bit

--- TJ432-PL.srcs/sources_1/new/I2S.v
+++ TJ432-PL.srcs/sources_1/new/I2S.v
@@ -22,10 +22,10 @@
 
 module I2S16bit(
     input CLK,
-    input RST, 
-    input [7:0] data_input,
-//    output ready,
+    input RST_n, 
+    input [15:0] data_input,
 
+    output MCLK,
     output LCRK,
     output BSCK,
     output TXD,
@@ -37,15 +37,14 @@ module I2S16bit(
 //BSCK（CLK）12分频
 //LCRK(BCLK)24分频
 
-reg mclk_reg = 1'b1;
 reg bsck_reg = 1'b1;
 reg lcrk_reg = 1'b1;
 reg txd_reg = 1'b0;
-reg [7:0] i2s_data = 8'b0; 
+reg [15:0] i2s_data = 16'b0; 
 reg data_clk_reg = 1'b0;
 
 
-//assign MCLK = mclk_reg;
+assign MCLK = CLK;
 assign BSCK = bsck_reg;
 assign LCRK = lcrk_reg;
 assign TXD = txd_reg;
@@ -56,9 +55,9 @@ assign DATA_CLK = data_clk_reg;
 //从CLK（?M）4分频获得BSCK(24BIT)
 reg [7:0] bsck_div = 8'd0;
 
-always@( posedge CLK ) //产生BCLK
+always@( posedge CLK or negedge RST_n ) //产生BCLK
 begin
-    if ( !RST )
+    if ( !RST_n )
     begin
         bsck_div <= 8'd0;
         bsck_reg <= 1'b1;
@@ -81,9 +80,9 @@ end
 reg [7:0] bsck_cnt = 8'd0; 
 //从BCLK23分频获得LCRK并放数据、
 
-always@ ( negedge BSCK ) //产生LRCK
+always@ ( negedge BSCK or negedge RST_n ) //产生LRCK
 begin
-    if ( !RST )
+    if ( !RST_n )
     begin
         bsck_cnt <= 8'd0;
         lcrk_reg <= 1'b1;
@@ -92,12 +91,12 @@ begin
     end
     else
     begin
-        if ( bsck_cnt < 8'd8 ) //0-7
+        if ( bsck_cnt < 8'd16 ) //0-15
         begin
             bsck_cnt <= bsck_cnt + 8'd1;    
             lcrk_reg <= lcrk_reg;
             data_clk_reg <= 1'b0;
-            txd_reg <= i2s_data[8'd7 - bsck_cnt];//放数据
+            txd_reg <= i2s_data[8'd15 - bsck_cnt];//放数据
             i2s_data <= i2s_data ;
         end
         else if ( bsck_cnt == 8'd23 )//反转LCRK
@@ -108,7 +107,7 @@ begin
             txd_reg <= 1'b0;          
             i2s_data <= data_input;
         end
-        else//8-21
+        else//16-21
         begin
             bsck_cnt <= bsck_cnt + 8'd1;
             lcrk_reg <= lcrk_reg;
