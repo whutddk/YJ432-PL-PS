a838bdc2e2af9af5fce88e3246b088578fb847a9
whutddk <295054118@whut.edu.cn>
2018-08-02 23:40:32 +0800

    载入polyphase数组

--- PL_repository/FANN-PL/TJ432-FANN.srcs/sources_1/new/subband.v
+++ PL_repository/FANN-PL/TJ432-FANN.srcs/sources_1/new/subband.v
@@ -33,6 +33,45 @@ input [31:0] gb;
 input [31:0] offset;
 input [31:0] oddBlock;
 
+parameter integer polyCoef[0:264] = {
+	/* shuffled vs. original from 0, 1, ... 15 to 0, 15, 2, 13, ... 14, 1 */
+	32'h00000000, 32'h00000074, 32'h00000354, 32'h0000072c, 32'h00001fd4, 32'h00005084, 32'h000066b8, 32'h000249c4,
+	32'h00049478, 32'hfffdb63c, 32'h000066b8, 32'hffffaf7c, 32'h00001fd4, 32'hfffff8d4, 32'h00000354, 32'hffffff8c,
+	32'hfffffffc, 32'h00000068, 32'h00000368, 32'h00000644, 32'h00001f40, 32'h00004ad0, 32'h00005d1c, 32'h00022ce0,
+	32'h000493c0, 32'hfffd9960, 32'h00006f78, 32'hffffa9cc, 32'h0000203c, 32'hfffff7e4, 32'h00000340, 32'hffffff84,
+	32'hfffffffc, 32'h00000060, 32'h00000378, 32'h0000056c, 32'h00001e80, 32'h00004524, 32'h000052a0, 32'h00020ffc,
+	32'h000491a0, 32'hfffd7ca0, 32'h00007760, 32'hffffa424, 32'h00002080, 32'hfffff6ec, 32'h00000328, 32'hffffff74,
+	32'hfffffffc, 32'h00000054, 32'h00000384, 32'h00000498, 32'h00001d94, 32'h00003f7c, 32'h00004744, 32'h0001f32c,
+	32'h00048e18, 32'hfffd6008, 32'h00007e70, 32'hffff9e8c, 32'h0000209c, 32'hfffff5ec, 32'h00000310, 32'hffffff68,
+	32'hfffffffc, 32'h0000004c, 32'h0000038c, 32'h000003d0, 32'h00001c78, 32'h000039e4, 32'h00003b00, 32'h0001d680,
+	32'h00048924, 32'hfffd43ac, 32'h000084b0, 32'hffff990c, 32'h00002094, 32'hfffff4e4, 32'h000002f8, 32'hffffff5c,
+	32'hfffffffc, 32'h00000044, 32'h00000390, 32'h00000314, 32'h00001b2c, 32'h0000345c, 32'h00002ddc, 32'h0001ba04,
+	32'h000482d0, 32'hfffd279c, 32'h00008a20, 32'hffff93a4, 32'h0000206c, 32'hfffff3d4, 32'h000002dc, 32'hffffff4c,
+	32'hfffffffc, 32'h00000040, 32'h00000390, 32'h00000264, 32'h000019b0, 32'h00002ef0, 32'h00001fd4, 32'h00019dc8,
+	32'h00047b1c, 32'hfffd0be8, 32'h00008ecc, 32'hffff8e64, 32'h00002024, 32'hfffff2c0, 32'h000002c0, 32'hffffff3c,
+	32'hfffffff8, 32'h00000038, 32'h0000038c, 32'h000001bc, 32'h000017fc, 32'h0000299c, 32'h000010e8, 32'h000181d8,
+	32'h0004720c, 32'hfffcf09c, 32'h000092b4, 32'hffff894c, 32'h00001fc0, 32'hfffff1a4, 32'h000002a4, 32'hffffff2c,
+	32'hfffffff8, 32'h00000034, 32'h00000380, 32'h00000120, 32'h00001618, 32'h00002468, 32'h00000118, 32'h00016644,
+	32'h000467a4, 32'hfffcd5cc, 32'h000095e0, 32'hffff8468, 32'h00001f44, 32'hfffff084, 32'h00000284, 32'hffffff18,
+	32'hfffffff8, 32'h0000002c, 32'h00000374, 32'h00000090, 32'h00001400, 32'h00001f58, 32'hfffff068, 32'h00014b14,
+	32'h00045bf0, 32'hfffcbb88, 32'h00009858, 32'hffff7fbc, 32'h00001ea8, 32'hffffef60, 32'h00000268, 32'hffffff04,
+	32'hfffffff8, 32'h00000028, 32'h0000035c, 32'h00000008, 32'h000011ac, 32'h00001a70, 32'hffffded8, 32'h00013058,
+	32'h00044ef8, 32'hfffca1d8, 32'h00009a1c, 32'hffff7b54, 32'h00001dfc, 32'hffffee3c, 32'h0000024c, 32'hfffffef0,
+	32'hfffffff4, 32'h00000024, 32'h00000340, 32'hffffff8c, 32'h00000f28, 32'h000015b0, 32'hffffcc70, 32'h0001161c,
+	32'h000440bc, 32'hfffc88d8, 32'h00009b3c, 32'hffff7734, 32'h00001d38, 32'hffffed18, 32'h0000022c, 32'hfffffedc,
+	32'hfffffff4, 32'h00000020, 32'h00000320, 32'hffffff1c, 32'h00000c68, 32'h0000111c, 32'hffffb92c, 32'h0000fc6c,
+	32'h00043150, 32'hfffc708c, 32'h00009bb8, 32'hffff7368, 32'h00001c64, 32'hffffebf4, 32'h00000210, 32'hfffffec4,
+	32'hfffffff0, 32'h0000001c, 32'h000002f4, 32'hfffffeb4, 32'h00000974, 32'h00000cb8, 32'hffffa518, 32'h0000e350,
+	32'h000420b4, 32'hfffc5908, 32'h00009b9c, 32'hffff6ff4, 32'h00001b7c, 32'hffffead0, 32'h000001f4, 32'hfffffeac,
+	32'hfffffff0, 32'h0000001c, 32'h000002c4, 32'hfffffe58, 32'h00000648, 32'h00000884, 32'hffff9038, 32'h0000cad0,
+	32'h00040ef8, 32'hfffc425c, 32'h00009af0, 32'hffff6ce0, 32'h00001a88, 32'hffffe9b0, 32'h000001d4, 32'hfffffe94,
+	32'hffffffec, 32'h00000018, 32'h0000028c, 32'hfffffe04, 32'h000002e4, 32'h00000480, 32'hffff7a90, 32'h0000b2fc,
+	32'h0003fc28, 32'hfffc2c90, 32'h000099b8, 32'hffff6a3c, 32'h00001988, 32'hffffe898, 32'h000001bc, 32'hfffffe7c,
+	32'h000001a0, 32'h0000187c, 32'h000097fc, 32'h0003e84c, 32'hffff6424, 32'hffffff4c, 32'h00000248, 32'hffffffec
+};
+
+
+
 reg [31:0] buf1[0:31];
 reg [31:0] buf2[0:31];
 
@@ -366,60 +405,60 @@ else begin
 	<= buf1[17] + buf1[25] + buf1[29];
 
 	
-	vbuf[0+ offset + (oddBlock ? VBUF_LENGTH  : 0)+64+64] 
-	<= vbuf[8+ offset + (oddBlock ? VBUF_LENGTH  : 0)+64+64] 
+	vbuf[0+ offset + (oddBlock ? VBUF_LENGTH  : 0)+64*2] 
+	<= vbuf[8+ offset + (oddBlock ? VBUF_LENGTH  : 0)+64*2] 
 	<= buf[ 9] + buf[13];
 	
-	vbuf[0+ offset + (oddBlock ? VBUF_LENGTH  : 0)+64+64+64] 
-	<= vbuf[8+ offset + (oddBlock ? VBUF_LENGTH  : 0)+64+64+64] 
+	vbuf[0+ offset + (oddBlock ? VBUF_LENGTH  : 0)+64*3] 
+	<= vbuf[8+ offset + (oddBlock ? VBUF_LENGTH  : 0)+64*3] 
 	<= buf[21] + buf[25] + buf[29];
 
-	vbuf[0+ offset + (oddBlock ? VBUF_LENGTH  : 0)+64+64+64+64] 
-	<= vbuf[8+ offset + (oddBlock ? VBUF_LENGTH  : 0)+64+64+64+64] 
+	vbuf[0+ offset + (oddBlock ? VBUF_LENGTH  : 0)+64*4] 
+	<= vbuf[8+ offset + (oddBlock ? VBUF_LENGTH  : 0)+64*4] 
 	<= buf[ 5];
 	
-	vbuf[0+ offset + (oddBlock ? VBUF_LENGTH  : 0)+64+64+64+64+64] 
-	<= vbuf[8+ offset + (oddBlock ? VBUF_LENGTH  : 0)+64+64+64+64+64] 
+	vbuf[0+ offset + (oddBlock ? VBUF_LENGTH  : 0)+64*5] 
+	<= vbuf[8+ offset + (oddBlock ? VBUF_LENGTH  : 0)+64*5] 
 	<= buf[21] + buf[29] + buf[27];
 
-	vbuf[0+ offset + (oddBlock ? VBUF_LENGTH  : 0)+64+64+64+64+64+64] 
-	<= vbuf[8+ offset + (oddBlock ? VBUF_LENGTH  : 0)+64+64+64+64+64+64] 
+	vbuf[0+ offset + (oddBlock ? VBUF_LENGTH  : 0)+64*6] 
+	<= vbuf[8+ offset + (oddBlock ? VBUF_LENGTH  : 0)+64*6] 
 	<= buf[13] + buf[11];
 		
-	vbuf[0+ offset + (oddBlock ? VBUF_LENGTH  : 0)+64+64+64+64+64+64+64] 
-	<= vbuf[8+ offset + (oddBlock ? VBUF_LENGTH  : 0)+64+64+64+64+64+64+64] 
+	vbuf[0+ offset + (oddBlock ? VBUF_LENGTH  : 0)+64*7] 
+	<= vbuf[8+ offset + (oddBlock ? VBUF_LENGTH  : 0)+64*7] 
 	<= buf[19] + buf[29] + buf[27];	
 			
-	vbuf[0+ offset + (oddBlock ? VBUF_LENGTH  : 0)+64+64+64+64+64+64+64+64] 
-	<= vbuf[8+ offset + (oddBlock ? VBUF_LENGTH  : 0)+64+64+64+64+64+64+64+64] 
+	vbuf[0+ offset + (oddBlock ? VBUF_LENGTH  : 0)+64*8] 
+	<= vbuf[8+ offset + (oddBlock ? VBUF_LENGTH  : 0)+64*8] 
 	<= buf[ 3];	
 		
-	vbuf[0+ offset + (oddBlock ? VBUF_LENGTH  : 0)+64+64+64+64+64+64+64+64+64] 
-	<= vbuf[8+ offset + (oddBlock ? VBUF_LENGTH  : 0)+64+64+64+64+64+64+64+64+64] 
+	vbuf[0+ offset + (oddBlock ? VBUF_LENGTH  : 0)+64*9] 
+	<= vbuf[8+ offset + (oddBlock ? VBUF_LENGTH  : 0)+64*9] 
 	<= buf[19] + buf[27] + buf[31];	
 
-	vbuf[0+ offset + (oddBlock ? VBUF_LENGTH  : 0)+64+64+64+64+64+64+64+64+64+64] 
-	<= vbuf[8+ offset + (oddBlock ? VBUF_LENGTH  : 0)+64+64+64+64+64+64+64+64+64+64] 
+	vbuf[0+ offset + (oddBlock ? VBUF_LENGTH  : 0)+64*10] 
+	<= vbuf[8+ offset + (oddBlock ? VBUF_LENGTH  : 0)+64*10] 
 	<= buf[11] + buf[15];	
 
-	vbuf[0+ offset + (oddBlock ? VBUF_LENGTH  : 0)+64+64+64+64+64+64+64+64+64+64+64] 
-	<= vbuf[8+ offset + (oddBlock ? VBUF_LENGTH  : 0)+64+64+64+64+64+64+64+64+64+64+64] 
+	vbuf[0+ offset + (oddBlock ? VBUF_LENGTH  : 0)+64*11] 
+	<= vbuf[8+ offset + (oddBlock ? VBUF_LENGTH  : 0)+64*11] 
 	<= buf[23] + buf[27] + buf[31];	
 
-	vbuf[0+ offset + (oddBlock ? VBUF_LENGTH  : 0)+64+64+64+64+64+64+64+64+64+64+64+64] 
-	<= vbuf[8+ offset + (oddBlock ? VBUF_LENGTH  : 0)+64+64+64+64+64+64+64+64+64+64+64+64] 
+	vbuf[0+ offset + (oddBlock ? VBUF_LENGTH  : 0)+64*12] 
+	<= vbuf[8+ offset + (oddBlock ? VBUF_LENGTH  : 0)+64*12] 
 	<= buf[ 7];	
 	
-	vbuf[0+ offset + (oddBlock ? VBUF_LENGTH  : 0)+64+64+64+64+64+64+64+64+64+64+64+64+64] 
-	<= vbuf[8+ offset + (oddBlock ? VBUF_LENGTH  : 0)+64+64+64+64+64+64+64+64+64+64+64+64+64] 
+	vbuf[0+ offset + (oddBlock ? VBUF_LENGTH  : 0)+64*13] 
+	<= vbuf[8+ offset + (oddBlock ? VBUF_LENGTH  : 0)+64*13] 
 	<= buf[23] + buf[31];	
 		
-	vbuf[0+ offset + (oddBlock ? VBUF_LENGTH  : 0)+64+64+64+64+64+64+64+64+64+64+64+64+64+64] 
-	<= vbuf[8+ offset + (oddBlock ? VBUF_LENGTH  : 0)+64+64+64+64+64+64+64+64+64+64+64+64+64+64] 
+	vbuf[0+ offset + (oddBlock ? VBUF_LENGTH  : 0)+64*14] 
+	<= vbuf[8+ offset + (oddBlock ? VBUF_LENGTH  : 0)+64*14] 
 	<= buf[15];	
 			
-	vbuf[0+ offset + (oddBlock ? VBUF_LENGTH  : 0)+64+64+64+64+64+64+64+64+64+64+64+64+64+64+64] 
-	<= vbuf[8+ offset + (oddBlock ? VBUF_LENGTH  : 0)+64+64+64+64+64+64+64+64+64+64+64+64+64+64+64] 
+	vbuf[0+ offset + (oddBlock ? VBUF_LENGTH  : 0)+64*15] 
+	<= vbuf[8+ offset + (oddBlock ? VBUF_LENGTH  : 0)+64*15] 
 	<= buf[31];
 
 
@@ -488,8 +527,31 @@ else begin
 	vbuf[0+ 16 + ((offset - oddBlock) & 7) + (oddBlock ? 0 : VBUF_LENGTH) + 64*15] 
 	<= vbuf[8+ 16 + ((offset - oddBlock) & 7) + (oddBlock ? 0 : VBUF_LENGTH) + 64*15] 
 	<= buf[16] + buf[24] + buf[28];
+
+//do not realize now
+	// 	if (es) 
+	// {
+	// 	d = dest + 64*16 + ((offset - oddBlock) & 7) + (oddBlock ? 0 : VBUF_LENGTH);
+	// 	s = d[0];	CLIP_2N(s, 31 - es);	d[0] = d[8] = (s << es);
 	
+	// 	d = dest + offset + (oddBlock ? VBUF_LENGTH  : 0);
+	// 	for (i = 16; i <= 31; i++) {
+	// 		s = d[0];	CLIP_2N(s, 31 - es);	d[0] = d[8] = (s << es);	d += 64;
+	// 	}
+
+	// 	d = dest + 16 + ((offset - oddBlock) & 7) + (oddBlock ? 0 : VBUF_LENGTH);
+	// 	for (i = 15; i >= 0; i--) {
+	// 		s = d[0];	CLIP_2N(s, 31 - es);	d[0] = d[8] = (s << es);	d += 64;
+	// 	}
+	// }
+
+
 	end // else if ( subband_clk_cnt == 8'd6 )
+
+	//polyphase
+	else if ( subband_clk_cnt == 8'd7 ) begin
+
+	end // else if ( subband_clk_cnt == 8'd7 )
 
 
 
