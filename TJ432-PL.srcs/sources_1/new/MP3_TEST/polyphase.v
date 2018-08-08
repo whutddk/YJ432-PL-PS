3f17a169f55cc25c237baddd9d0212d1c5ccc3c1
WUT_Ruige_Lee <295054118@whut.edu.cn>
2018-08-08 13:57:18 +0800

    搭建polyphase

--- TJ432-PL.srcs/sources_1/new/MP3_TEST/polyphase.v
+++ TJ432-PL.srcs/sources_1/new/MP3_TEST/polyphase.v
@@ -21,12 +21,48 @@
 
 module polyphase 
 (
-	input clk,    // Clock
+	input CLK,    // Clock
 	input RST_n,  // Asynchronous reset active low
 	
+	//ram operate
+	output reg [11:0] Ram_addrA,
+	output reg [11:0] Ram_addrB,
+	input [31:0] Ram_dataA,
+	input [31:0] Ram_dataB,
 
-	
+	//Rom operate
+	output reg [8:0] Rom_addrA,
+	output reg [8:0] Rom_addrB,
+	input [31:0] Rom_dataA,
+	input [31:0] Rom_dataB,	
+
+	//FIFO pcm DATA
+	output reg [31:0] fifo_data,
+	output reg fifo_clk
+
+
 );
+
+reg [31:0] pcm[0:64];
+reg [7:0] poly_cnt = 8'd0; 
+always @( posedge CLK or negedge RST_n ) begin
+if ( !RST_n ) begin
+	Ram_addrA <= 12'b0;
+	Ram_addrB <= 12'b0;
+	Rom_addrA <= 9'b0;
+	Rom_addrB <= 9'b0;
+	fifo_data <= 32'd0;
+	fifo_clk <= 1'b0;
+
+	poly_cnt <= 8'd0;
+end
+
+else begin
+	poly_cnt <= poly_cnt + 8'd1;
+end
+
+end
+
 
 endmodule
 
