3972e62d8445d804b19ff7e005c160805ed9fa96
WUT_Ruige_Lee <295054118@whut.edu.cn>
2018-08-08 14:55:55 +0800

    复用乘加器

--- PL_repository/FANN-PL/TJ432-FANN.srcs/sources_1/new/MP3_TEST/polyphase.v
+++ PL_repository/FANN-PL/TJ432-FANN.srcs/sources_1/new/MP3_TEST/polyphase.v
@@ -24,6 +24,8 @@ module polyphase
 	input CLK,    // Clock
 	input RST_n,  // Asynchronous reset active low
 	
+	input [2:0] subband_state,
+
 	//ram operate
 	output reg [11:0] Ram_addrA,
 	output reg [11:0] Ram_addrB,
@@ -38,10 +40,115 @@ module polyphase
 
 	//FIFO pcm DATA
 	output reg [31:0] fifo_data,
-	output reg fifo_clk
+	output reg fifo_clk,
 
+	inout [63:0] sum1L_pre_S1,
+	inout [31:0] mult1L_A_S1,
+	inout [31:0] mult1L_B_S1,
+	input [63:0] mult_out1L_S1,
 
+	inout [63:0] sum1L_pre_S2,
+	inout [31:0] mult1L_A_S2,
+	inout [31:0] mult1L_B_S2,
+	input [63:0] mult_out1L_S2,
+
+	inout [63:0] sum2L_pre_S1,
+	inout [31:0] mult2L_A_S1,
+	inout [31:0] mult2L_B_S1,
+	input [63:0] mult_out2L_S1,
+
+	inout [63:0] sum2L_pre_S2,
+	inout [31:0] mult2L_A_S2,
+	inout [31:0] mult2L_B_S2,
+	input [63:0] mult_out2L_S2,
+
+	inout [63:0] sum1R_pre_S1,
+	inout [31:0] mult1R_A_S1,
+	inout [31:0] mult1R_B_S1,
+	input [63:0] mult_out1R_S1,
+
+	inout [63:0] sum1R_pre_S2,
+	inout [31:0] mult1R_A_S2,
+	inout [31:0] mult1R_B_S2,
+	input [63:0] mult_out1R_S2,
+
+	inout [63:0] sum2R_pre_S1,
+	inout [31:0] mult2R_A_S1,
+	inout [31:0] mult2R_B_S1,
+	input [63:0] mult_out2R_S1,
+
+	inout [63:0] sum2R_pre_S2,
+	inout [31:0] mult2R_A_S2,
+	inout [31:0] mult2R_B_S2,
+	input [63:0] mult_out2R_S2,
 );
+
+reg [63:0] sum1L_pre_S1_reg;
+reg [31:0] mult1L_A_S1_reg;
+reg [31:0] mult1L_B_S1_reg;
+
+reg [63:0] sum1L_pre_S2_reg;
+reg [31:0] mult1L_A_S2_reg;
+reg [31:0] mult1L_B_S2_reg;
+
+reg [63:0] sum2L_pre_S1_reg;
+reg [31:0] mult2L_A_S1_reg;
+reg [31:0] mult2L_B_S1_reg;
+
+reg [63:0] sum2L_pre_S2_reg;
+reg [31:0] mult2L_A_S2_reg;
+reg [31:0] mult2L_B_S2_reg;
+
+reg [63:0] sum1R_pre_S1_reg;
+reg [31:0] mult1R_A_S1_reg;
+reg [31:0] mult1R_B_S1_reg;
+
+reg [63:0] sum1R_pre_S2_reg;
+reg [31:0] mult1R_A_S2_reg;
+reg [31:0] mult1R_B_S2_reg;
+
+reg [63:0] sum2R_pre_S1_reg;
+reg [31:0] mult2R_A_S1_reg;
+reg [31:0] mult2R_B_S1_reg;
+
+reg [63:0] sum2R_pre_S2_reg;
+reg [31:0] mult2R_A_S2_reg;
+reg [31:0] mult2R_B_S2_reg;
+
+//conntion
+assign sum1L_pre_S1 = ( subband_state !=  ) ? 64'bz : sum1L_pre_S1_reg;
+assign mult1L_A_S1 = ( subband_state !=  ) ? 32'bz : mult1L_A_S1_reg;
+assign mult1L_B_S1 = ( subband_state !=  ) ? 32'bz : mult1L_B_S1_reg;
+
+assign sum1L_pre_S2 = ( subband_state !=  ) ? 64'bz : sum1L_pre_S2_reg;
+assign mult1L_A_S2 = ( subband_state !=  ) ? 32'bz : mult1L_A_S2_reg;
+assign mult1L_B_S2 = ( subband_state !=  ) ? 32'bz : mult1L_B_S2_reg;
+
+assign sum2L_pre_S1 = ( subband_state !=  ) ? 64'bz : sum2L_pre_S1_reg;
+assign mult2L_A_S1 = ( subband_state !=  ) ? 32'bz : mult2L_A_S1_reg;
+assign mult2L_B_S1 = ( subband_state !=  ) ? 32'bz : mult2L_B_S1_reg;
+
+assign sum2L_pre_S2 = ( subband_state !=  ) ? 64'bz : sum2L_pre_S2_reg;
+assign mult2L_A_S2 = ( subband_state !=  ) ? 32'bz : mult2L_A_S2_reg;
+assign mult2L_B_S2 = ( subband_state !=  ) ? 32'bz : mult2L_B_S2_reg;
+
+assign sum1R_pre_S1 = ( subband_state !=  ) ? 64'bz : sum1R_pre_S1_reg;
+assign mult1R_A_S1 = ( subband_state !=  ) ? 32'bz : mult1R_A_S1_reg;
+assign mult1R_B_S1 = ( subband_state !=  ) ? 32'bz : mult1R_B_S1_reg;
+
+assign sum1R_pre_S2 = ( subband_state !=  ) ? 64'bz : sum1R_pre_S2_reg;
+assign mult1R_A_S2 = ( subband_state !=  ) ? 32'bz : mult1R_A_S2_reg;
+assign mult1R_B_S2 = ( subband_state !=  ) ? 32'bz : mult1R_B_S2_reg;
+
+assign sum2R_pre_S1 = ( subband_state !=  ) ? 64'bz : sum2R_pre_S1_reg;
+assign mult2R_A_S1 = ( subband_state !=  ) ? 32'bz : mult2R_A_S1_reg;
+assign mult2R_B_S1 = ( subband_state !=  ) ? 32'bz : mult2R_B_S1_reg;
+
+assign sum2R_pre_S2 = ( subband_state !=  ) ? 64'bz : sum2R_pre_S2_reg;
+assign mult2R_A_S2 = ( subband_state !=  ) ? 32'bz : mult2R_A_S2_reg;
+assign mult2R_B_S2 = ( subband_state !=  ) ? 32'bz : mult2R_B_S2_reg;
+
+
 
 reg [31:0] pcm[0:64];
 reg [7:0] poly_cnt = 8'd0; 
