6da81a756e4959d1dd9514083a592574fa55ce7e
WUT_Ruige_Lee <295054118@whut.edu.cn>
2018-08-30 16:34:29 +0800

    添加一些宏定义

--- TJ432-PL.srcs/sources_1/new/Fann/fann_net.v
+++ TJ432-PL.srcs/sources_1/new/Fann/fann_net.v
@@ -47,11 +47,29 @@ module fann_net (
 parameter INIT_STATE = 1'b0;
 parameter WORK_STATE = 1'b1;
 
-
 reg state_initialize = INIT_STATE;
 reg [31:0] init_cnt = 32'd0;
 reg [7:0] layer_cnt = 8'd0;
 reg [7:0] neure_cnt = 8'd0;
+
+
+/* USER Patameter */
+`define LAYER_NUM  8'd9 //10 layer
+
+`define NEURE_LAY0 8'd8	//input layer:number of input
+`define NEURE_LAY1 8'd50
+`define NEURE_LAY2 8'd50
+`define NEURE_LAY3 8'd50
+`define NEURE_LAY4 8'd50
+`define NEURE_LAY5 8'd50
+`define NEURE_LAY6 8'd50
+`define NEURE_LAY7 8'd50
+`define NEURE_LAY8 8'd50
+`define NEURE_LAY9 8'd4	//output layer:number of output
+
+`define MAX_BANDWIDTH 	50
+
+
 
 
 always @( posedge CLK or negedge RST_n ) begin
@@ -67,7 +85,24 @@ always @( posedge CLK or negedge RST_n ) begin
 
 		end // if ( state_initialize == INIT_STATE )
 
-		else begin
+		else begin //( state_initialize == WORK_STATE )
+
+			// /* Layer state machine Start*/
+			// if ( layer_cnt == 8'd0 ) begin	//input layer
+
+			// end // if ( layer_cnt == 8'd0 )
+
+			// else if ( layer_cnt >= LAYER_NUM ) begin //Final Layer
+
+			// end // else if ( layer_cnt == LAYER_NUM ) //Final Layer
+
+			// else begin // Hiden Layer
+
+			// end // else // Hiden Layer
+
+			// /* Layer state machine End */
+
+
 
 		end // else ( state_initialize == WORK_STATE )
 
