333ddc661e4eceeaaa1a0b2cfe5762b24b853035
WUT_Ruige_Lee <295054118@qq.com>
2019-01-11 16:41:35 +0800

    link bug fix

--- PS_repository/demo/demo_flexbus_register/main.cpp
+++ PS_repository/demo/demo_flexbus_register/main.cpp
@@ -1,10 +1,11 @@
+
 /*
 * @File Name main.cpp
 * @File Path M:\MAS2\YJ432-PL-PS\PS_repository\demo\demo_flexbus_register\main.cpp
 * @Author: WUT_Ruige_Lee
 * @Date:   2019-01-04 19:54:48
 * @Last Modified by:   WUT_Ruige_Lee
-* @Last Modified time: 2019-01-11 16:23:34
+* @Last Modified time: 2019-01-11 16:40:45
 * @Email: 295054118@whut.edu.cn"
 */
 
@@ -13,7 +14,7 @@
 #include "PL_def.h"
 #include "mcu_cfg.h"
 
-extern void Flexbus_init();
+extern void flexbus_init();
 
 
 int main(void)
@@ -21,7 +22,7 @@ int main(void)
 	wait_fpga_init();
 
 	//flexbus initialization
-	Flexbus_init();
+	flexbus_init();
 
 	*(LED_FRE_REG) = 1000000;
 	wait(0.05);
@@ -29,34 +30,34 @@ int main(void)
 	while(1)
 	{		
 
-		*(BZ_FRE_REG) = 1000000;
+		*(BZ_FRE_REG) = 500000;
 		*(RED_DUTY_REG) = 100000;
 		*(GREEN_DUTY_REG) = 100000;
 		*(BLUE_DUTY_REG) = 900000;		
 		wait(1);
 
-		*(BZ_FRE_REG) = 2000000;
+		*(BZ_FRE_REG) = 1000000;
 		*(RED_DUTY_REG) = 100000;
 		*(GREEN_DUTY_REG) = 900000;
+		*(BLUE_DUTY_REG) = 100000;		
+		wait(1);
+
+		*(BZ_FRE_REG) = 2000000;
+		*(RED_DUTY_REG) = 900000;
+		*(GREEN_DUTY_REG) = 100000;
 		*(BLUE_DUTY_REG) = 100000;		
 		wait(1);
 
 		*(BZ_FRE_REG) = 3000000;
-		*(RED_DUTY_REG) = 900000;
-		*(GREEN_DUTY_REG) = 100000;
-		*(BLUE_DUTY_REG) = 100000;		
-		wait(1);
-
-		*(BZ_FRE_REG) = 4000000;
-		*(RED_DUTY_REG) = 900000;
-		*(GREEN_DUTY_REG) = 900000;
+		*(RED_DUTY_REG) = 700000;
+		*(GREEN_DUTY_REG) = 800000;
 		*(BLUE_DUTY_REG) = 900000;		
 		wait(1);
 
-		*(BZ_FRE_REG) = 5000000;
-		*(RED_DUTY_REG) = 100000;
+		*(BZ_FRE_REG) = 6000000;
+		*(RED_DUTY_REG) = 500000;
 		*(GREEN_DUTY_REG) = 100000;
-		*(BLUE_DUTY_REG) = 100000;		
+		*(BLUE_DUTY_REG) = 700000;		
 		wait(1);
 
 
