e292589ade5624d14f3802080bc5c289cc5bab7e
WUT_Ruige_Lee <295054118@qq.com>
2019-01-12 16:22:32 +0800

    嵌入flexbus硬件结构剥离

--- PL_repository/Template/FB_3inPID.v
+++ PL_repository/Template/FB_3inPID.v
@@ -1,23 +1,28 @@
-`timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: WUT
+// Company:  WUT 
 // Engineer: WUT RUIGE LEE
-// 
 // Create Date: 2018/06/21 17:44:39
-// Design Name: 
+// Last Modified by:   WUT_Ruige_Lee
+// Last Modified time: 2019-01-12 16:20:36
+// Email: 295054118@whut.edu.cn
+// Design Name:   
 // Module Name: FB_3inPID
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
+// Project Name:   
+// Target Devices:   
+// Tool Versions:   
+// Description:   
 // 
-// Dependencies: 
+// Dependencies:   
 // 
-// Revision:
+// Revision:  
 // Revision 0.01 - File Created
-// Additional Comments:
+// Additional Comments:  
 // 
 //////////////////////////////////////////////////////////////////////////////////
+
+
+
+`timescale 1ns / 1ps
 
 
 module FB_3inPID(
