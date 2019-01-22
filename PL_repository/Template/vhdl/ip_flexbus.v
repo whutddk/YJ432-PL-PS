c81d9543c33d5079aa4ebf68367afe98f8fdf064
Ruige_Lee <295054118@qq.com>
2019-01-22 11:42:51 +0800

    完成flexbus 组合逻辑编写

--- PL_repository/Template/vhdl/ip_flexbus.v
+++ PL_repository/Template/vhdl/ip_flexbus.v
@@ -2,8 +2,8 @@
 // Company: wuhan university of technology
 // Engineer: RUIGE LEE
 // Create Date: 2018/07/19 10:35:01
-// Last Modified by:   WUT_Ruige_Lee
-// Last Modified time: 2019-01-16 16:14:30
+// Last Modified by:   Ruige_Lee
+// Last Modified time: 2019-01-22 11:42:17
 // Email: 295054118@whut.edu.cn
 // Design Name: 
 // Module Name: ip_flexbus
@@ -153,135 +153,219 @@ basic_reg # (32)
 	.qout(LEDB_Puty_Qout)
 );
 
+wire baseAddressCheck;
+assign baseAddressCheck = ((FB_AD[31:0] & 32'hf0000000) == ( FB_BASE[31:0] & 32'hf0000000 )) ? 1'b1:1'b0;
+
+
+assign ip_ADDR_Din = (
+						{32{FB_ALE}} & 
+						(
+							{32{baseAddressCheck}} &
+							(
+								FB_AD[31:0];
+							)
+							|
+							{32{~baseAddressCheck}} &
+							(
+								32'b0;
+							)
+						)
+					)
+					|
+					(
+						{32{~FB_ALE}} &
+						(
+							ip_ADDR_Qout;
+						)
+					);
+
+assign ADDR_COMF_Din = (
+							(FB_ALE) & 
+							(
+								(baseAddressCheck) &
+								(
+									1'b1
+								)
+								|
+								(~baseAddressCheck) &
+								(
+									1'b0;
+								)
+							)
+						)
+						|
+						(	(~FB_ALE) &
+							(
+								ADDR_COMF_Qout
+							)
+						);
+
+always @(*) begin 
+	if((~FB_ALE & ADD_COMF & ~FB_CS & FB_RW))
+		casez( ip_ADDR & 32'h0fffffff )
+			32'b00000: begin
+				FB_AD_Din <= LED_FREQ_Qout;
+			end // 32'b00000:
+			32'b00100:begin
+				FB_AD_Din <= BZ_FREQ_Qout;
+			end // 32'b00100:
+			32'b01000:begin
+				FB_AD_Din <= LEDR_Puty_Qout;
+			end // 32'b01000:
+			32'b01100:begin
+				FB_AD_Din <= LEDG_Puty_Qout;
+			end // 32'b01100:
+			32'b10000:begin
+				FB_AD_Din <= LEDB_Puty_Qout;
+			end // 32'b10000:
+
+			default:begin
+			end			
+		endcase
+
+	else
+		FB_AD_Din <= FB_AD_Qout;
+end
+
+assign LED_FREQ_Din = ( (~FB_ALE & ADD_COMF & ~FB_CS & ~FB_RW) & 
+						(ip_ADDR & 32'h0fffffff == 32'b00000) )
+						? FB_AD
+						: LED_FREQ_Qout;
+
+assign BZ_FREQ_Din  = ( (~FB_ALE & ADD_COMF & ~FB_CS & ~FB_RW) & 
+						(ip_ADDR & 32'h0fffffff == 32'b00100) )
+						? FB_AD
+						: BZ_FREQ_Qout;					
+
+assign LEDR_Puty_Din = ( (~FB_ALE & ADD_COMF & ~FB_CS & ~FB_RW) & 
+						(ip_ADDR & 32'h0fffffff == 32'b01000) )
+						? FB_AD
+						: LEDR_Puty_Qout;
+
+assign LEDG_Puty_Din = ( (~FB_ALE & ADD_COMF & ~FB_CS & ~FB_RW) & 
+						(ip_ADDR & 32'h0fffffff == 32'b01100) )
+						? FB_AD
+						: LEDG_Puty_Qout;
+
+assign LEDB_Puty_Din = 	( (~FB_ALE & ADD_COMF & ~FB_CS & ~FB_RW) & 
+						(ip_ADDR & 32'h0fffffff == 32'b10000) )
+						? FB_AD
+						: LEDB_Puty_Qout;			
 
 
 
 
+// always@( negedge FB_CLK or negedge RST_n )  begin
+// 	if ( !RST_n ) begin
 
-
-
-
-
-
-
-always@( negedge FB_CLK or negedge RST_n )  begin
-	if ( !RST_n ) begin
-
-		ip_ADDR[31:0] <= 32'b0;
-		ADD_COMF <= 1'b0;
+// 		ip_ADDR[31:0] <= 32'b0;
+// 		ADD_COMF <= 1'b0;
 		
-		FB_AD_reg[31:0] <= 32'b0;
- 
-      
-		LED_FREQ_Reg <= 32'b0;
-		BZ_FREQ_Reg <= 32'b0;
-		LEDR_Puty_Reg <= 32'b0;
-		LEDG_Puty_Reg <= 32'b0;
-		LEDB_Puty_Reg <= 32'b0;  
+// 		FB_AD_reg[31:0] <= 32'b0;
+
+// 		LED_FREQ_Reg <= 32'b0;
+// 		BZ_FREQ_Reg <= 32'b0;
+// 		LEDR_Puty_Reg <= 32'b0;
+// 		LEDG_Puty_Reg <= 32'b0;
+// 		LEDB_Puty_Reg <= 32'b0;  
 		
-	end
-	else begin
+// 	end
+// 	else begin
 
-		FB_AD_reg <= FB_AD_reg;
+		// FB_AD_reg <= FB_AD_reg;
 
-		ip_ADDR <= ip_ADDR;
+		// ip_ADDR <= ip_ADDR;
 
-		ADD_COMF <= ADD_COMF;
+		// ADD_COMF <= ADD_COMF;
 
-		LED_FREQ_Reg <= LED_FREQ_Reg;
-		BZ_FREQ_Reg <= BZ_FREQ_Reg;
-		LEDR_Puty_Reg <= LEDR_Puty_Reg;
-		LEDG_Puty_Reg <= LEDG_Puty_Reg;
-		LEDB_Puty_Reg <= LEDB_Puty_Reg;
+		// LED_FREQ_Reg <= LED_FREQ_Reg;
+		// BZ_FREQ_Reg <= BZ_FREQ_Reg;
+		// LEDR_Puty_Reg <= LEDR_Puty_Reg;
+		// LEDG_Puty_Reg <= LEDG_Puty_Reg;
+		// LEDB_Puty_Reg <= LEDB_Puty_Reg;
 
 		 
-		if ( FB_ALE == 1'b1 ) begin  //flexbus_address latch enable
-//            AD_TRI <= 1'b1; //  FB_ALE == 1'B1 && FB_CS = X && FB_RW == X && ADD_COMF == X
+		// if ( FB_ALE == 1'b1 ) begin  //flexbus_address latch enable
 
+			// if ( (FB_AD[31:0] & 32'hf0000000) == ( FB_BASE[31:0] & 32'hf0000000 ) ) begin// check base address 
+				// ADD_COMF <= 1'b1;
+				// ip_ADDR[31:0] <= FB_AD[31:0];
 
-			if ( (FB_AD[31:0] & 32'hf0000000) == ( FB_BASE[31:0] & 32'hf0000000 ) ) begin// check base address 
-				ADD_COMF <= 1'b1;
-				ip_ADDR[31:0] <= FB_AD[31:0];
+			// end
+			// else begin // IN ADDRESS LATCH MODE BUT THE ADDRESS IS NOT SELECT THIS FLEXBUS IP
+				// ADD_COMF <= 1'b0;
+				// ip_ADDR[31:0] <= 32'b0;
+			// end
+		// end // FB_ALE == 1'b1
 
-			end
-			else begin // IN ADDRESS LATCH MODE BUT THE ADDRESS IS NOT SELECT THIS FLEXBUS IP
-				ADD_COMF <= 1'b0;
-				ip_ADDR[31:0] <= 32'b0;
-			end
-		end // FB_ALE == 1'b1
-		else begin //FB_ALE == 1'B0
-			if ( ADD_COMF == 1'b1 ) begin //ADDRESS CONFIRM
-				if ( FB_CS == 1'b0 ) begin //CS is enable 
-					if ( FB_RW == 1'b0 ) begin  //in write mode
-					
-//                        AD_TRI <= 1'b1; //  FB_ALE == 1'B0 && FB_CS == 1'b0 && FB_RW == 1'b0 && ADD_COMF == 1'b1
-						
-						casez( ip_ADDR & 32'h0fffffff )
+		// else begin //FB_ALE == 1'B0
+			// if ( ADD_COMF == 1'b1 ) begin //ADDRESS CONFIRM
+				// if ( FB_CS == 1'b0 ) begin //CS is enable 
+					// if ( FB_RW == 1'b0 ) begin  //in write mode
+											
+						// casez( ip_ADDR & 32'h0fffffff )
 												
-							32'b00000: begin
-								LED_FREQ_Reg[31:0] <= FB_AD[31:0];
-							end
-							32'b00100:begin
-								BZ_FREQ_Reg[31:0] <= FB_AD[31:0];
-							end
-							32'b01000:begin
-								LEDR_Puty_Reg[31:0] <= FB_AD[31:0];
-							end
-							32'b01100:begin
-								LEDG_Puty_Reg[31:0] <= FB_AD[31:0];
-							end
-							32'b10000:begin
-								LEDB_Puty_Reg[31:0] <= FB_AD[31:0];
-							end
+							// 32'b00000: begin
+								// LED_FREQ_Reg[31:0] <= FB_AD[31:0];
+							// end
+							// 32'b00100:begin
+								// BZ_FREQ_Reg[31:0] <= FB_AD[31:0];
+							// end
+							// 32'b01000:begin
+								// LEDR_Puty_Reg[31:0] <= FB_AD[31:0];
+							// end
+							// 32'b01100:begin
+								// LEDG_Puty_Reg[31:0] <= FB_AD[31:0];
+							// end
+							// 32'b10000:begin
+								// LEDB_Puty_Reg[31:0] <= FB_AD[31:0];
+							// end
 							
 							
-							32'h0780zzzz:begin
+							// 32'h0780zzzz:begin
 								
-							end
+							// end
 
-							default:begin
-							end // default:
-						endcase
-					end // FB_RW == 1'b0
+							// default:begin
+							// end // default:
+						// endcase
+					// end // FB_RW == 1'b0
 					
-					else if ( FB_RW == 1'b1 ) begin //in read mode
-//                        AD_TRI <= 1'b0;     //  FB_ALE == 1'B0 && FB_CS == 1'b0 && FB_RW == 1'b1 && ADD_COMF == 1'b1
+					// else if ( FB_RW == 1'b1 ) begin //in read mode
 						
-						casez( ip_ADDR & 32'h0fffffff )
-							32'b00000: begin
-								FB_AD_reg[31:0] <= LED_FREQ_Reg[31:0];
-							end // 32'b00000:
-							32'b00100:begin
-								FB_AD_reg[31:0] <= BZ_FREQ_Reg[31:0];
-							end // 32'b00100:
-							32'b01000:begin
-								FB_AD_reg[31:0] <= LEDR_Puty_Reg[31:0];
-							end // 32'b01000:
-							32'b01100:begin
-								FB_AD_reg[31:0] <= LEDG_Puty_Reg[31:0];
-							end // 32'b01100:
-							32'b10000:begin
-								FB_AD_reg[31:0] <= LEDB_Puty_Reg[31:0];
-							end // 32'b10000:
+						// casez( ip_ADDR & 32'h0fffffff )
+							// 32'b00000: begin
+								// FB_AD_reg[31:0] <= LED_FREQ_Reg[31:0];
+							// end // 32'b00000:
+							// 32'b00100:begin
+								// FB_AD_reg[31:0] <= BZ_FREQ_Reg[31:0];
+							// end // 32'b00100:
+							// 32'b01000:begin
+								// FB_AD_reg[31:0] <= LEDR_Puty_Reg[31:0];
+							// end // 32'b01000:
+							// 32'b01100:begin
+								// FB_AD_reg[31:0] <= LEDG_Puty_Reg[31:0];
+							// end // 32'b01100:
+							// 32'b10000:begin
+								// FB_AD_reg[31:0] <= LEDB_Puty_Reg[31:0];
+							// end // 32'b10000:
 
-							default:begin
-							end // default:
-						endcase
-					end // FB_RW == 1'b1
-					else begin
-					end // else
-				end  // ( FB_CS == 1'b0 )
-				else begin //( FB_CS == 1'b1 )
-//                    AD_TRI <= 1'b1; //  FB_ALE == 1'B0 && FB_CS == 1'b1 && FB_RW == X && ADD_COMF == 1'b1
-				end
-			end //ADDRESS CONFIRM
-			else begin //ADDRESS VOTE
-//                AD_TRI <= 1'b1; //  FB_ALE == 1'B0 && FB_CS = X && FB_RW == X && ADD_COMF == 1'b0
-			end //ADDRESS VOTE
-		end // FB_ALE == 1'B0
-	end
+							// default:begin
+							// end // default:
+						// endcase
+					// end // FB_RW == 1'b1
+					// else begin
+					// end // else
+				// end  // ( FB_CS == 1'b0 )
+				// else begin //( FB_CS == 1'b1 )
+				// end
+			// end //ADDRESS CONFIRM
+			// else begin //ADDRESS VOTE
+			// end //ADDRESS VOTE
+		// end // FB_ALE == 1'B0
+	// end
 
-end
+// end
  
 	
 	
