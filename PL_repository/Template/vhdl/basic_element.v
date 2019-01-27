8ea33b1534612bcca8c5f63c74da087dd548fcf2
Ruige_Lee <295054118@whut.edu.cn>
2019-01-27 16:24:10 +0800

    复位处最好不挂仿真延迟

--- PL_repository/Template/vhdl/basic_element.v
+++ PL_repository/Template/vhdl/basic_element.v
@@ -3,7 +3,7 @@
 // Engineer: WUT_Ruige_Lee
 // Create Date: 2019-01-16 15:52:29
 // Last Modified by:   Ruige_Lee
-// Last Modified time: 2019-01-24 18:07:26
+// Last Modified time: 2019-01-27 16:23:26
 // Email: 295054118@whut.edu.cn
 // Design Name:   
 // Module Name: basic_element
@@ -39,7 +39,7 @@ assign qout = qout_Reg;
 always @( posedge CLK or negedge RSTn ) begin
 
 	if ( !RSTn ) begin
-		qout_Reg <= #1 { DW { 1'b0 } };
+		qout_Reg <= { DW { 1'b0 } };
 	end
 
 	else begin
@@ -71,7 +71,7 @@ assign qout = qout_Reg;
 always @( negedge CLK or negedge RSTn ) begin
 
 	if ( !RSTn ) begin
-		qout_Reg <= #1 { DW { 1'b0 } };
+		qout_Reg <= { DW { 1'b0 } };
 	end
 
 	else begin
