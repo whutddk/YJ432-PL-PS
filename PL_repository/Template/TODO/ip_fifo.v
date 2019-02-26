f27e6ab15d24454e38286d4f42d924243f305901
Ruige_Lee <295054118@whut.edu.cn>
2019-02-26 11:38:16 +0800

    写完了

--- PL_repository/Template/TODO/ip_fifo.v
+++ PL_repository/Template/TODO/ip_fifo.v
@@ -3,7 +3,7 @@
 // Engineer: Ruige_Lee
 // Create Date: 2019-02-25 21:35:20
 // Last Modified by:   Ruige_Lee
-// Last Modified time: 2019-02-26 10:41:47
+// Last Modified time: 2019-02-26 11:37:38
 // Email: 295054118@whut.edu.cn
 // Design Name:   
 // Module Name: ip_fifo
@@ -86,15 +86,22 @@ basic_reg_clk_p # (
 		.dout(w_pr_Qout)
 );
 
-w_pr_Din =	( {3{w_pr_state0}} & grayCode1 )
-			| ( {3{w_pr_state1}} & grayCode2 )
-			| ( {3{w_pr_state2}} & grayCode3 )
-			| ( {3{w_pr_state3}} & grayCode4 )
-			| ( {3{w_pr_state4}} & grayCode5 )
-			| ( {3{w_pr_state5}} & grayCode6 )
-			| ( {3{w_pr_state6}} & grayCode7 )
-			| ( {3{w_pr_state7}} & grayCode0 );
-
+w_pr_Din =	(
+				{3{fifoFull}} & w_pr_Qout
+			)
+			|
+			(	{3{~fifoFull}} &
+				(
+					( {3{w_pr_state0}} & grayCode1 )
+					| ( {3{w_pr_state1}} & grayCode2 )
+					| ( {3{w_pr_state2}} & grayCode3 )
+					| ( {3{w_pr_state3}} & grayCode4 )
+					| ( {3{w_pr_state4}} & grayCode5 )
+					| ( {3{w_pr_state5}} & grayCode6 )
+					| ( {3{w_pr_state6}} & grayCode7 )
+					| ( {3{w_pr_state7}} & grayCode0 )
+				)
+			);
 
 
 wire [2:0] w_pr_sync_Din;
@@ -132,15 +139,23 @@ basic_reg_clk_p # (
 );
 
 
-r_pr_Din =	( {3{r_pr_state0}} & grayCode1 )
-			| ( {3{r_pr_state1}} & grayCode2 )
-			| ( {3{r_pr_state2}} & grayCode3 )
-			| ( {3{r_pr_state3}} & grayCode4 )
-			| ( {3{r_pr_state4}} & grayCode5 )
-			| ( {3{r_pr_state5}} & grayCode6 )
-			| ( {3{r_pr_state6}} & grayCode7 )
-			| ( {3{r_pr_state7}} & grayCode0 );
-
+r_pr_Din =	(
+				{3{fifoEmpty}} & r_pr_Qout
+			)
+			| 
+			(
+				{3{~fifoEmpty}} &
+				(
+					( {3{r_pr_state0}} & grayCode1 )
+					| ( {3{r_pr_state1}} & grayCode2 )
+					| ( {3{r_pr_state2}} & grayCode3 )
+					| ( {3{r_pr_state3}} & grayCode4 )
+					| ( {3{r_pr_state4}} & grayCode5 )
+					| ( {3{r_pr_state5}} & grayCode6 )
+					| ( {3{r_pr_state6}} & grayCode7 )
+					| ( {3{r_pr_state7}} & grayCode0 )
+				)
+			);
 
 
 wire [2:0] r_pr_sync_Din;
@@ -201,43 +216,86 @@ assign fifo_empty_Din = ( r_pr_Qout == w_pr_sync_Qout ) ? 1'b1 : 1'b0;
 
 
 
-
-
-
-
 ///////////////////////////////////////////////////
-
-
-
-
-
-
+wire w_pr_state0 = (w_pr_Qout == grayCode0) ? 1'b1 : 1'b0;
+wire w_pr_state1 = (w_pr_Qout == grayCode1) ? 1'b1 : 1'b0;
+wire w_pr_state2 = (w_pr_Qout == grayCode2) ? 1'b1 : 1'b0;
+wire w_pr_state3 = (w_pr_Qout == grayCode3) ? 1'b1 : 1'b0;
+wire w_pr_state4 = (w_pr_Qout == grayCode4) ? 1'b1 : 1'b0;
+wire w_pr_state5 = (w_pr_Qout == grayCode5) ? 1'b1 : 1'b0;
+wire w_pr_state6 = (w_pr_Qout == grayCode6) ? 1'b1 : 1'b0;
+wire w_pr_state7 = (w_pr_Qout == grayCode7) ? 1'b1 : 1'b0;
 
 genvar i;
 generate
 	
+	wire [ dataWidth - 1 : 0 ] fifo_reg_Din[ 0 : fifoDepth - 1 ];
+	wire [ dataWidth - 1 : 0 ] fifo_reg_Qout[ 0 : fifoDepth - 1 ];
 
 	for ( i = 0; i < fifoDepth ; i = i + 1 ) begin
+
 		basic_reg_clk_p # (
-		.DW(16)
-	)
+			.DW(dataWidth)
+		)
+		fifo_reg
+		(
+			.CLK(w_clk),
+			.RSTn(RST_n),
 
-	(
-		input CLK,
-		input RSTn,
+			.din(fifo_reg_Din[i]),
+			.qout(fifo_reg_Qout[i])
+		);
 
-		input  [ DW-1:0 ] din,
-		output [ DW-1:0 ] qout
-);
+
+
 	end
+
+	assign fifo_reg_Din[0] = ( {DW{ (( w_pr_state0 | w_pr_state4 ) & (~fifoFull)) }} & w_data )
+							| ( {DW{ ((~( w_pr_state0 & w_pr_state4 )) | ( fifoFull )) }} & fifo_reg_Qout[0] );
+
+	assign fifo_reg_Din[1] = ( {DW{ (( w_pr_state1 | w_pr_state5 ) & (~fifoFull)) }} & w_data )
+							| ( {DW{ ((~( w_pr_state1 & w_pr_state5 )) | ( fifoFull )) }} & fifo_reg_Qout[1] );
+								
+	assign fifo_reg_Din[2] = ( {DW{ ( (w_pr_state2 | w_pr_state6 ) & (~fifoFull)) }} & w_data )
+							| ( {DW{ ((~( w_pr_state2 & w_pr_state6 )) | ( fifoFull )) }} & fifo_reg_Qout[2] );
+
+	assign fifo_reg_Din[3] = ( {DW{ ( (w_pr_state3 | w_pr_state7 ) & (~fifoFull)) }} & w_data )
+							| ( {DW{ ((~( w_pr_state3 & w_pr_state7 )) | ( fifoFull )) }} & fifo_reg_Qout[3] );
 
 
 endgenerate
 
+wire r_pr_state0 = (r_pr_Qout == grayCode0) ? 1'b1 : 1'b0;
+wire r_pr_state1 = (r_pr_Qout == grayCode1) ? 1'b1 : 1'b0;
+wire r_pr_state2 = (r_pr_Qout == grayCode2) ? 1'b1 : 1'b0;
+wire r_pr_state3 = (r_pr_Qout == grayCode3) ? 1'b1 : 1'b0;
+wire r_pr_state4 = (r_pr_Qout == grayCode4) ? 1'b1 : 1'b0;
+wire r_pr_state5 = (r_pr_Qout == grayCode5) ? 1'b1 : 1'b0;
+wire r_pr_state6 = (r_pr_Qout == grayCode6) ? 1'b1 : 1'b0;
+wire r_pr_state7 = (r_pr_Qout == grayCode7) ? 1'b1 : 1'b0;
 
 
-
-
+assign r_data = (
+					{DW{fifoEmpty}} & {DW{1'b0}}
+				)
+				|
+				(
+					{DW{~fifoEmpty}} & 
+					(
+						(
+							{DW{(r_pr_state0 | r_pr_state4)}} & fifo_reg_Qout[0]
+						)
+						|(
+							{DW{(r_pr_state1 | r_pr_state5)}} & fifo_reg_Qout[1]
+						)
+						|(
+							{DW{(r_pr_state2 | r_pr_state6)}} & fifo_reg_Qout[2]
+						)
+						|(
+							{DW{(r_pr_state3 | r_pr_state7)}} & fifo_reg_Qout[3]
+						)
+					)
+				);
 endmodule
 
 
